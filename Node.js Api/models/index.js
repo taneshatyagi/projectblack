d275d23116f20f6d518f85e513fc4ab4f7c0eacd
taneshatyagi <126880456+taneshatyagi@users.noreply.github.com>
2023-03-03 19:21:34 +0530

    Create index.js

---
+++ Node.js Api/models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Data = require('./data');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to MongoDB database');
+});
+
+module.exports = {
+  Data,
+};
